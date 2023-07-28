2af2a3cab6abe182ffc904b076b5b48ead4d9ce5
DeepakBarwal <deepakbarwal98@gmail.com>
2023-07-28 21:44:18 +0530

    brought in custom css assets and created Header & Footer components

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,32 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { FaShoppingCart, FaUser, FaShopify } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand
+            href="/"
+            className="d-flex align-items-center justify-content-center"
+          >
+            <FaShopify size={40} /> MERN eCommerce
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/cart">
+                <FaShoppingCart /> Cart
+              </Nav.Link>
+              <Nav.Link href="/login">
+                <FaUser /> Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
