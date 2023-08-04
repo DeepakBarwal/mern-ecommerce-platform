31fbc3f78a2dc014b674f94de895942b10db2394
DeepakBarwal <deepakbarwal98@gmail.com>
2023-08-04 10:59:08 +0530

    Added redux store and api slice

--- backend/src/controllers/products/productController.js
+++ backend/src/controllers/products/productController.js
@@ -3,11 +3,17 @@ import { ProductService } from "../../services/index.js";
 
 const productService = new ProductService();
 
+// @desc    Fetch All Products
+// @route   GET /products
+// @access  Public
 export const getAllProducts = asyncHandler(async (req, res) => {
   const products = await productService.getAll();
   res.json({ status: "success", data: products });
 });
 
+// @desc    Fetch a Product
+// @route   GET /products/:id
+// @access  Public
 export const getProductById = asyncHandler(async (req, res) => {
   const product = await productService.get(req.params.id);
 
