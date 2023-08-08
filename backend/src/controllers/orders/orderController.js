4d9a4292ed10d58ef685e2d927713f0637b09432
DeepakBarwal <deepakbarwal98@gmail.com>
2023-08-08 15:29:52 +0530

    Create order done

--- backend/src/controllers/orders/orderController.js
+++ backend/src/controllers/orders/orderController.js
@@ -7,21 +7,68 @@ const orderService = new OrderService();
 // @route   POST /orders
 // @access  Private
 export const addOrderItems = asyncHandler(async (req, res) => {
-  res.json("add order items");
+  let {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error("No order items");
+  } else {
+    orderItems = orderItems.map((order) => ({
+      ...order,
+      product: order._id,
+      _id: undefined,
+    }));
+    const order = await orderService.create({
+      orderItems,
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+
+    res.status(201).json({
+      status: "success",
+      data: order,
+    });
+  }
 });
 
 // @desc    Get logged in user's orders
 // @route   GET /orders/myorders
 // @access  Private
 export const getMyOrders = asyncHandler(async (req, res) => {
-  res.json("get my orders");
+  const orders = await orderService.getMyOrders(req.user._id);
+  res.status(200).json({
+    status: "success",
+    data: orders,
+  });
 });
 
 // @desc    Get order by id
 // @route   GET /orders/:id
 // @access  Private
 export const getOrderById = asyncHandler(async (req, res) => {
-  res.json("get order by id");
+  const order = await orderService.getOrderByIDAndPopulate(req.params.id);
+  if (order) {
+    res.status(200).json({
+      status: "success",
+      data: order,
+    });
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
 });
 
 // @desc    Update order to paid
--- backend/src/services/orders/orderService.js
+++ backend/src/services/orders/orderService.js
@@ -5,6 +5,29 @@ class OrderService extends CrudRepository {
   constructor() {
     super(Order);
   }
+
+  async getMyOrders(userId) {
+    try {
+      const orders = await Order.find({ user: userId });
+      return orders;
+    } catch (error) {
+      console.error(`Error at Order Service layer: ${error}`);
+      throw error;
+    }
+  }
+
+  async getOrderByIDAndPopulate(orderId) {
+    try {
+      const order = await Order.findById(orderId).populate(
+        "user",
+        "name email"
+      );
+      return order;
+    } catch (error) {
+      console.error(`Error at Order Service layer: ${error}`);
+      throw error;
+    }
+  }
 }
 
 export default OrderService;
