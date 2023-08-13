f3b83226dfda38bc62a52705534ab8fa3e882f4c
DeepakBarwal <deepakbarwal98@gmail.com>
2023-08-13 20:06:48 +0530

    Added backend api url as env var

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,6 +1,6 @@
-export const BASE_URL = "";
-export const PRODUCTS_URL = "/products";
-export const USERS_URL = "/users";
-export const ORDERS_URL = "/orders";
-export const PAYPAL_URL = "/config/paypal";
-export const UPLOADS_URL = "/uploads";
+export const BASE_URL = `${process.env.REACT_APP_BACKEND_API_URL}`;
+export const PRODUCTS_URL = `${BASE_URL}/products`;
+export const USERS_URL = `${BASE_URL}/users`;
+export const ORDERS_URL = `${BASE_URL}/orders`;
+export const PAYPAL_URL = `${BASE_URL}/config/paypal`;
+export const UPLOADS_URL = `${BASE_URL}/uploads`;
