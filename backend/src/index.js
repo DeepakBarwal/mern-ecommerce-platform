4ba80095f5bdfba030fa88058acfa0af28c262b2
DeepakBarwal <deepakbarwal98@gmail.com>
2023-08-13 23:02:36 +0530

    changed path of index

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,8 +29,9 @@ if (NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
   // any route that's not api will be redirected to index.html
+  console.log(path.resolve(__dirname, "frontend", "build", "index.html"));
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => res.send("API is running..."));
